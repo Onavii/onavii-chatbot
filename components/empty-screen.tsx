1c1b50a12248a88ce1b9f714e793df99154f1a3c
Nour Amer <84950398+NoureldinAmer@users.noreply.github.com>
2023-11-26 19:28:59 -0700

    fix error

--- components/empty-screen.tsx
+++ components/empty-screen.tsx
@@ -24,7 +24,7 @@ export function EmptyScreen({ setInput }: Pick<UseChatHelpers, 'setInput'>) {
     <div className="mx-auto max-w-2xl px-4">
       <div className="rounded-lg border bg-background p-8">
         <h1 className="mb-2 text-lg font-semibold">
-          Welcome to Onavii's AI assistant
+          Welcome to Onavii&apos;s AI assistant
         </h1>
         <p className="mb-2 leading-normal text-muted-foreground">
           Your expert guide for neuromuscular disease support and healthcare
